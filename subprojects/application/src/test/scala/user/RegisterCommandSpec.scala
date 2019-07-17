da7aa6d69bb46ace6bd70e9dc45b2516cc49467b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-17 23:53:27 +0100

    added reset in db

--- subprojects/application/src/test/scala/user/RegisterCommandSpec.scala
+++ subprojects/application/src/test/scala/user/RegisterCommandSpec.scala
@@ -3,20 +3,17 @@ package application.test.user
 import application.user.{BuildRegisterRequest, RegisterCommand}
 import infrastructure.persistence.user.{UserPersistentModel, UserRepository, UserSchema}
 import infrastructure.test.persistence.Exec
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FunSuite}
 import slick.jdbc.MySQLProfile.api._
 import slick.lifted.TableQuery
 
-class RegisterCommandSpec extends FunSuite with Exec {
+class RegisterCommandSpec extends FunSuite with BeforeAndAfterEach with BeforeAndAfterAll with Exec {
 
   val userSchema = TableQuery[UserSchema]
   val registerCommand = new RegisterCommand(
     userRepository = new UserRepository()
   )
-
-
-
-
+  
   test("Can register a non existing user") {
     val givenRegisterRequest = BuildRegisterRequest.any(
       withSurname = "my_username"
@@ -29,5 +26,14 @@ class RegisterCommandSpec extends FunSuite with Exec {
     assert(rows.size === 1)
     assert(rows.isInstanceOf[Vector[_]])
     assert(rows.head.isInstanceOf[UserPersistentModel])
+  }
+
+  override def beforeEach() {
+    exec(userSchema.schema.dropIfExists)
+    exec(userSchema.schema.create)
+  }
+
+  override def afterAll() = {
+    dbConnection.close()
   }
 }
